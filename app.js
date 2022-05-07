21f9add160f1276065475e5f3f5be59a2d9e848f
Ben <ben@daveclark.co.nz>
2022-05-07 12:27:38 +1200

    simplify env vars for multiple users on different processes

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const axios = require('axios');
 const { execSync } = require("child_process");
 require('dotenv').config();
 
-// for debugging
+// for debugging - skip running cypress
 const SKIP_CYPRESS = process.env?.SKIP_CYPRESS ?? false;
 
-const DISCORD_ENABLED = true;
 const cycleTimeInSeconds = 7666 * 1000; // every 2hrs and 10mins-ish
 
-const discordKey = process.env.DISCORD_KEY;
-const cypressRecordKey = process.env.CYPRESS_RECORD_KEY;
+const testRunCommand = 'npm run cy:run -- --spec "cypress/integration/test.spec.js"';
 
-const baseRunCommand = 'npm run cy:run -- --spec "cypress/integration/make-transaction.spec.js"';
-const runCommand = baseRunCommand + " --record --key " + cypressRecordKey;
-
-const testRunCommand =
-  'npm run cy:run -- --spec "cypress/integration/test.spec.js"';
+const { discordKey, runCommand } = getEnvironemnt();
 
 
 (async () => {
@@ -47,13 +41,39 @@ async function main() {
 
 
 /**
+ * Returns environment variables with default values if not set
+ * Will throw error if required variable is missing
+ */
+function getEnvironemnt() {
+
+  const username = process.env.COTPS_USERNAME;
+  const password = process.env.COTPS_PASSWORD;
+  const cypressRecordKey = process.env.CYPRESS_RECORD_KEY; // optional - disable cypress recording
+  const discordKey = process.env.DISCORD_KEY; // optional - disable discord logs
+
+  if (!username || !password) {
+    throw new Error('Username and password are required');
+  }
+
+  const baseRunCommand = `npm run cy:run -- --env username=${username},password=${password} --spec "cypress/integration/make-transaction.spec.js"`;
+  const runCommand = cypressRecordKey 
+    ? baseRunCommand + " --record --key " + cypressRecordKey 
+    : baseRunCommand;
+
+  return {
+    runCommand,
+    discordKey
+  };
+}
+
+/**
  * Sends a POST request message to discord
  *
  * @param {string|object} message
  */
  async function logToDiscord(message) {
 
-	if (!DISCORD_ENABLED) { return null; }
+	if (!discordKey) { return null; }
 
 	if (!message) { throw new Error('No message content'); }
 
